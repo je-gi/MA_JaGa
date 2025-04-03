9662626df3b735f5b9e2646d3bc4bd89f687bfd8
je-gi <jael.gangl@gmail.com>
2025-04-03 17:14:45 +0200

    P2: prefab duplicator

---
+++ Assets/02_Scripts/P2/PrefabDuplicator.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Interactors;
+
+public class PrefabDuplicator : MonoBehaviour
+{
+    public XRSocketInteractor mainSocketInteractor;
+    public XRSocketInteractor variantSocketInteractor;
+    public Transform spawnPoint;
+    public GameObject defaultPrefab;
+    public GameObject variantPrefab1;
+    public GameObject variantPrefab2;
+    public GameObject variantPrefab3;
+    public GameObject variantPrefab4;
+
+    private GameObject selectedPrefab;
+
+    private void Start()
+    {
+        selectedPrefab = defaultPrefab;
+    }
+
+    public void OnButtonPressed()
+    {
+        if (mainSocketInteractor.hasSelection)
+        {
+            if (variantSocketInteractor.hasSelection)
+            {
+                var selectedInteractable = variantSocketInteractor.GetOldestInteractableSelected();
+
+                if (selectedInteractable != null)
+                {
+                    GameObject variantObject = selectedInteractable.transform.gameObject;
+
+                    if (variantObject.CompareTag("Variant01"))
+                    {
+                        selectedPrefab = variantPrefab1;
+                    }
+                    else if (variantObject.CompareTag("Variant02"))
+                    {
+                        selectedPrefab = variantPrefab2;
+                    }
+                    else if (variantObject.CompareTag("Variant03"))
+                    {
+                        selectedPrefab = variantPrefab3;
+                    }
+                    else if (variantObject.CompareTag("Variant04"))
+                    {
+                        selectedPrefab = variantPrefab4;
+                    }
+                }
+            }
+            else
+            {
+                selectedPrefab = defaultPrefab;
+            }
+
+            Instantiate(selectedPrefab, spawnPoint.position, spawnPoint.rotation);
+        }
+    }
+}
